74961eac8b4d7e20800178b33159fd99766d6b21
Ryan Zenoni <Ryan@Lisa-Zenonis-MacBook-Pro.local>
2014-02-12 23:30:40 -0700

    Working On KingCrab

--- projects/critters/KingCrab.java
+++ projects/critters/KingCrab.java
@@ -2,5 +2,26 @@ public class KingCrab extends CrabCritter {
 	public KingCrab() {
 
 	}
+
+	public void processActors(ArrayList<Actor> actors) {
+        Grid<Actor> gr = getGrid();
+        Location originalLoc = new Location(getLocation());
+        int originalDir = locs.getDirection();
+        for (int i = 0; i<actors.length(); i++) {
+        	Actor actor = actors[i];
+        	Location loc = new Location(actor.getLocation());
+        	int dir = loc.getDirection();
+       		for (Actor a : actors) {
+            	if (!(a instanceof Actor)) {
+                	Location next = loc.getAdjacentLocation(originalDir);
+                	if (gr.isValid()) {
+                		moveTo(next);
+                	} else {
+                		removeSelfFromGrid();
+                	}
+            	}
+        	}
+        }        
+    }
 	
 }
